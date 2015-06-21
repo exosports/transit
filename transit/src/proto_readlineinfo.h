0b566e747ad02e947cb08d8311cee5cc166cb555
pcubillos <pcubillos@knights.ucf.edu>
2015-06-20 22:26:56 -0400

    In proto_readlineinfo.h: removed outdated functions, added datafileBS.
    In readlineinfo.c: Re-factored datafileBS() to include the linear
    search after the binary search, and added argument to search upwards
    or downwards.  Re-factored readdatarng() to read the current TLI
    version (binary search for each isotope in line-transition data).

--- transit/src/proto_readlineinfo.h
+++ transit/src/proto_readlineinfo.h
@@ -4,17 +4,17 @@
 #define P_(s) ()
 #endif
 
-/* src/readlineinfo.c */
+/* src/readlineinfo.c                                                       */
+extern void datafileBS P_((FILE *fp, PREC_NREC offs, PREC_NREC nfields,
+                           PREC_LNDATA target, PREC_NREC *resultp,
+                           int reclength, int up));
 extern int readtli_bin P_((FILE *fp, struct transit *tr, struct lineinfo *li));
-extern int readtli_ascii P_((FILE *fp, struct transit *tr, struct lineinfo *li));
-extern int getinifinasctli P_((double *ini, double *fin, FILE *fp, char *file));
 extern int setimol P_((struct transit *tr));
 extern int checkrange P_((struct transit *tr, struct lineinfo *li));
 extern int readinfo_tli P_((struct transit *tr, struct lineinfo *li));
 extern int readdatarng P_((struct transit *tr, struct lineinfo *li));
 extern int readlineinfo P_((struct transit *tr));
 extern int freemem_isotopes P_((struct isotopes *iso, long *pi));
-extern int freemem_lineinfotrans P_((struct lineinfo *li, long *pi));
 extern int freemem_lineinfo P_((struct lineinfo *li, long *pi));
 extern int freemem_linetransition P_((struct line_transition *lt, long *pi));
 extern void saveline P_((FILE *fp, struct lineinfo *li));
