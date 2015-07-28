5b11c45338b4702efc0eb052560d304764285e71
pcubillos <pcubillos@knights.ucf.edu>
2015-07-28 17:37:30 -0400

    Edited HITRAN cross-section format script to handle multiple input files. Fixed some style issues.

--- scripts/HITRAN_csx_format.py
+++ scripts/HITRAN_csx_format.py
@@ -2,94 +2,111 @@
 
 import numpy as np
 import sys, os
+import scipy.constants as sc
+
+# Amagat (in molec cm^-3):
+N0 = sc.physical_constants[
+        "Loschmidt constant (273.15 K, 101.325 kPa)"][0] * 1e-6
 
 def main():
-    """
-    This scripts reformats a HITRAN cross section file into the
-    format read by the Transit program. Designed for use with
-    Hargreaves, et al (2014) cross section files.
-    
-    Usage
-    -----
-    Run from the shell:
-    $ ./HITRAN_csx_format.py <inputfile> <outputfile>
+  """
+  This scripts reformats a HITRAN cross-section file into the
+  format read by the Transit program. Designed for use with
+  Hargreaves, et al (2014) cross section files.
 
-    Examples
-    --------
-    $ ./HITRAN_csx_format.py CH4_023C_XSC.xsc methane_296K.dat
-    
-    Modification History
-    --------------------
-    2015-07-23 rchallen Initial implementation.
-    """
-    infile, outfile = sys.argv[1:]
-    # Open file for reading
-    f_in = open(infile, 'r')
+  Usage
+  -----
+  Run from the shell:
+  $ ./HITRAN_csx_format.py <inputfile> <outputfile>
 
-    ntoa = 2.6867805e19 # Conversion factor from number density (in cm-3)
-                        # to amagats
-    
-    # Read in the header and split into fields
+  Examples
+  --------
+  $ ./HITRAN_csx_format.py CH4_023C_XSC.xsc methane_296K.dat
+  """
+  infile  = sys.argv[1:-1]
+  outfile = sys.argv[-1]
+
+  # There is one temperature sample per file:
+  ntemp = nfiles = len(infile)
+  # Array of sampled temperatures:
+  temp = np.zeros(nfiles, np.double)
+
+  # Number of data points per line:
+  linelen = 10
+
+  for i in np.arange(nfiles):
+    # Open file for reading:
+    f_in = open(infile[i], 'r')
+
     line = str.split(f_in.readline())
+    # Set the fields from the header:
+    if i == 0:
+      mol     = str(  line[0]) # Molecule designation (eg CH4)
+      wn_init = float(line[1]) # Initial wavenumber (cm-1)
+      wn_fin  = float(line[2]) # Final wavenumber (cm-1)
+      nwave   = int(  line[3]) # Number of data points
+      press   = float(line[5]) # Pressure (Torr)
+      max_cs  = float(line[6]) # Maximum cross section (cm2 molecule-1)
+      res     = float(line[7]) # Resolution of measurement (cm-1)
+      molname = str(  line[8]) # Full molecule name (eg Methane)
 
-    # Set the fields from the header
-    mol     = str(  line[0]) # Molecule designation (eg CH4)
-    wn_init = float(line[1]) # Initial wavenumber (cm-1)
-    wn_fin  = float(line[2]) # Final wavenumber (cm-1)
-    ndata   = int(  line[3]) # Number of data points
-    temp    = float(line[4]) # Temperature (K)
-    press   = float(line[5]) # Pressure (Torr)
-    max_cs  = float(line[6]) # Maximum cross section (cm2 molecule-1)
-    res     = float(line[7]) # Resolution of measurement (cm-1)
-    molname = str(  line[8]) # Full molecule name (eg Methane)
+      # Array to be filled with cross section data
+      data = np.zeros((nwave, ntemp), np.double)
+    else:
+      # Check that the data from the other files match:
+      if str(line[0]) != mol:
+        print("Can't combine files for different species.")
+        return
 
-    # Number of data points per line
-    linelen = 10
-
-    # Array to be filled with cross section data
-    data = np.zeros(ndata)
-
-    # Wavenumber array to be printed in output file
-    wn   = np.linspace(wn_init, wn_fin, ndata)
+    temp[i] = line[4]  # Add temperature (K) to list
 
     # For counting loop repetitions
     j = 0
 
     # Loop through the lines of data
     for x in f_in:
-        # Loop through fields in each line
-        for i in range(linelen):
-            # Only read data that is there (don't read extra fields at the end)
-            if i+linelen*j < ndata:
-              data[i+linelen*j] = str.split(x)[i]
-        # Increment number of loop repetitions
-        j +=1
+      # Loop through fields in each line
+      for k in range(linelen):
+        # Only read data that is there (don't read extra fields at the end)
+        if k+linelen*j < nwave:
+          data[k+linelen*j, i] = str.split(x)[k]
+      # Increment number of loop repetitions
+      j += 1
 
-    data *= ntoa # Convert to correct units
-        
     f_in.close()
 
-    # Open file for writing
-    f_out = open(outfile, 'w')
+  # Convert from [FINDME] to cm-1 amagat-1 units:
+  data *= N0
 
-    # Write the header
-    f_out.write('# This file is a reformatted version of Hargreaves et al \n')
-    f_out.write('# (2015) hot methane cross section file \n \n')
+  # Wavenumber array to be printed in output file
+  wn = np.linspace(wn_init, wn_fin, nwave)
 
-    # Write the molecule and temperature lines
-    f_out.write('i ' + mol       + '\n')
-    f_out.write('t ' + str(temp) + '\n')
+  # Open file for writing:
+  f_out = open(outfile, 'w')
 
-    # Write comment on units
-    f_out.write('\n # Wavenumber in cm-1, cross section in cm-1 amagat-1 \n')
+  # Write the header
+  f_out.write('# This file is a reformatted version of Hargreaves et al\n')
+  f_out.write('# (2015) hot methane cross section file\n\n')
 
-    # Write data in format required by Transit program
-    for i in range(ndata):
-      f_out.write("{:.2f}".format(wn[i]) + '   ' + "{:.4e}".format(data[i]) + '\n')
+  # Write the molecule and temperature lines
+  f_out.write('i {:s}\n'.format(mol))
+  f_out.write('t ')
+  for j in np.arange(ntemp):
+    f_out.write(' {:6.1f}'.format(temp[j]))
+  f_out.write('\n\n')
 
-    f_out.close()
-    
+  # Write comment on units
+  f_out.write('\n # Wavenumber in cm-1, cross section in cm-1 amagat-1\n')
 
-    
+  # Write data in format required by Transit program
+  for i in range(nwave):
+    f_out.write(" {:7.2f}   ".format(wn[i]))
+    for j in np.arange(ntemp):
+      f_out.write(" {:.3e}".format(data[i,j]))
+    f_out.write('\n')
+
+  f_out.close()
+
+
 if __name__ == "__main__":
-    main()
+  main()
