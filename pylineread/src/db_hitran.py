Corrected hitran TIPS partition function calculation (statistical weight).

pylineread/src/db_hitran.py
@@ -13,7 +13,7 @@ from driver import dbdriver
 #db_hitran.py dir:
 DBHdir = os.path.dirname(os.path.realpath(__file__))
 
-# CTIPS
+# TIPS
 sys.path.append(DBHdir + "/pytips")
 import pytips as tips
 
@@ -99,7 +99,7 @@ class hitran(dbdriver):
 
     Notes:
     ------
-    This function is a wrapper of the CTIPS library written by
+    This function is a wrapper of the TIPS library written by
     Patricio Cubillos, which is itself a C implementation of the
     Fortran TIPS routine by R. R. Gamache.
     The range of temperatures is limited to: 70K -- 3000K.
@@ -147,9 +147,8 @@ class hitran(dbdriver):
       mol = np.repeat(molID, len(Temp))
       iso = np.repeat(int(self.isotopes[i]), len(Temp))
 
-      # Call CTIPS with the given arrays:
+      # Call TIPS with the given arrays:
       PF[i] = tips.tips(mol, iso, Temp)
-      PF[i] /= self.gi[i]
 
     return Temp, PF
 
@@ -222,7 +221,7 @@ class hitran(dbdriver):
 
     Notes:
     ------
-    This function is a wrapper of the CTIPS library written by
+    This function is a wrapper of the TIPS library written by
     Patricio Cubillos, which is itself a C implementation of the
     Fortran TIPS routine by R. R. Gamache.
     The range of temperatures is limited to: 70K -- 3000K.