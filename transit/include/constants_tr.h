5e52a7c6cbf11e49286e42ea2b496762573f052b
Michael Himes <mhimes@knights.ucf.edu>
2020-08-20 16:20:53 -0400

    Updated constants

--- transit/include/constants_tr.h
+++ transit/include/constants_tr.h
@@ -20,19 +20,19 @@
 #define RHOSTP (1.29e-3)         /* Density at standard temp and press       */
 #define PI (3.141592653589793)   /* PI                                       */
 #define DEGREES (PI/180.0)       /* to make degrees to radian                */
-#define GGRAV (6.67430e-8)       /* Gravitational constant (erg * cm / g^2)  */
+#define GGRAV (6.673e-8)         /* Gravitational constant (erg * cm / g^2)  */
 #define HOUR (3600.0)            /* 1 hour (s)                               */
-#define AU (14959787070000.000)  /* 1 Astronomical unit (cm)                 */
+#define AU (14959786896040.492)  /* 1 Astronomical unit (cm)                 */
 #define ANGSTROM (1e-8)          /* 1 Angstrom (cm)                          */
 #define SUNMASS (1.9891e33)      /* Solar mass (g)                           */
-#define SUNRADIUS (6.957e10)     /* solar volumetric mean radius (cm)        */
-#define AMU (1.6605390666e-24)   /* Atomic Mass unit (g)                     */
+#define SUNRADIUS (6.95508e10)   /* solar volumetric mean radius (cm)        */
+#define AMU (1.66053886e-24)     /* Atomic Mass unit (g)                     */
 #define LO (2.686763e19)         /* Loschmidt constant (cm-3)                */
 #define EC (4.8032068e-10)       /* electronic charge (statcoulomb)          */
 #define LS (2.99792458e10)       /* Light Speed (cm / s)                     */
-#define ME (9.1093837015e-28)    /* Electron mass (g)                        */
-#define KB (1.380649e-16)        /* Boltzmann constant (erg / K)             */
-#define H (6.62607015e-27)       /* Planck's constant (erg * s)              */
+#define ME (9.1093897e-28)       /* Electron mass (g)                        */
+#define KB (1.380658e-16)        /* Boltzmann constant (erg / K)             */
+#define H (6.6260755e-27)        /* Planck's constant (erg * s)              */
 #define HC (H*LS)                /* for lower energy conversion (erg * cm)   */
 #define SIGCTE (PI*EC*EC/LS/LS/ME/AMU) /* Cross-sec constant (cm / g)        */
 #define EXPCTE (H*LS/KB)         /* Exponent constant (cm * K)               */
